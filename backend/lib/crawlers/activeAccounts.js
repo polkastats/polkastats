f09b9a87664d70614cae5b98c6396c8fe0ed77d7
mariopino <mariopino@protonmail.com>
2021-06-08 10:29:45 +0200

    refactor active accounts crawler

--- backend/lib/crawlers/activeAccounts.js
+++ backend/lib/crawlers/activeAccounts.js
@@ -1,12 +1,9 @@
 // @ts-check
 const { ApiPromise, WsProvider } = require('@polkadot/api');
-const { zip } = require('lodash');
 const pino = require('pino');
-const { wait } = require('../utils.js');
+const { dbParamInsert } = require('../utils.js');
 
-const DEFAULT_POLLING_TIME_MS = 1 * 60 * 1000;
 const logger = pino();
-
 const loggerOptions = {
   crawler: 'activeAccounts',
 };
@@ -16,169 +13,101 @@ const getAccountId = (account) => account
   .map(([e]) => e)
   .map((e) => e.toHuman());
 
-const fetchBlockNumber = async (api) => {
-  const { block } = await api.rpc.chain.getBlock();
-  return block.header.number.toNumber();
-};
-
 const fetchAccountIds = async (api) => getAccountId(await api.query.system.account.keys());
 
-const fetchAccountIdentity = async (accountId, api) => {
-  const info = await api.derive.accounts.info(accountId);
-  return info.identity;
-};
-
-const fetchAccountBalance = async (accountId, api) => {
-  const balance = await api.derive.balances.all(accountId);
-  return balance;
-};
-
 /**
- * Creates an account object from attributes.
  *
- * @param {String}  id         ID of the account
- * @param {String}  identity   Identity on the blockchain of the account
- * @param {String}  balances   Current balances of the account
- * @param {Boolean} isStaking  True if the account is staking
+ * @param {*} wsProviderUrl     Substrate node WS
+ * @param {*} pool              Postgres connection pool
  */
-const buildAccount = (id, identity, balances, isStaking) => ({
-  id, identity, balances, isStaking,
-});
-
-/**
- * Applies a zip function to multiple lists containing related data and then
- * groups related entries using a builder function.
- *
- * @param {String}    name      Name of the attribute to store the builder result
- * @param {Function}  builder   Function using to build the initial state object
- */
-const prepareState = (name, builder) => (itemsList) => zip(...itemsList)
-  .map((items) => builder(...items))
-  .map((result) => ({ [name]: result }));
-
-/**
- * Specific version of prepareState to initizalize the state with an account
- * object.
- */
-const makeState = prepareState('account', buildAccount);
-
-/**
- * Builds an upsert query: Try to INSERT an entry on the database, if a
- * conflict with the "account_id" key occurs, then update the entry instead of
- * create a new one. Stores the built query in the state object.
- *
- * @param {Object} state       State object, containing account info
- * @param {Number} block       Block number attached to the entry
- * @param {Number} timestamp   Timestamp attached to the entry
- */
-const makeQuery = (state, block, timestamp) => {
-  const {
-    id, identity, balances,
-  } = state.account;
-  const availableBalance = balances.availableBalance.toString();
-  const freeBalance = balances.freeBalance.toString();
-  const lockedBalance = balances.lockedBalance.toString();
-  const identityDisplay = identity.display ? identity.display.toString() : '';
-  const identityDisplayParent = identity.displayParent ? identity.displayParent.toString() : '';
-  const JSONIdentity = identity.display ? JSON.stringify(identity) : '';
-  const JSONbalances = JSON.stringify(balances);
-  const nonce = balances.accountNonce.toString();
-  const query = `
-    INSERT INTO   account (account_id, identity, identity_display, identity_display_parent, balances, available_balance, free_balance, locked_balance, nonce, timestamp, block_height)
-    VALUES        ('${id}', '${JSONIdentity}', '${identityDisplay}', '${identityDisplayParent}', '${JSONbalances}', '${availableBalance}', '${freeBalance}', '${lockedBalance}', '${nonce}', '${timestamp}', '${block}')
-    ON CONFLICT   (account_id)
-    DO UPDATE
-    SET           identity = EXCLUDED.identity,
-                  identity_display = EXCLUDED.identity_display,
-                  identity_display_parent = EXCLUDED.identity_display_parent,
-                  balances = EXCLUDED.balances,
-                  available_balance = EXCLUDED.available_balance,
-                  free_balance = EXCLUDED.free_balance,
-                  locked_balance = EXCLUDED.locked_balance,
-                  nonce = EXCLUDED.nonce,
-                  timestamp = EXCLUDED.timestamp,
-                  block_height = EXCLUDED.block_height;
-  `;
-
-  return { ...state, query };
-};
-
-/**
- * Uses a provided connection pool to send a query to Postgres. Stores a promise
- * wrapping the result of the execution in the state object.
- *
- * @param {Object}  state   State object, containing the query
- * @param {Object}  pool    Postgres connection pool
- */
-const execQuery = (state, pool) => {
-  const queryResult = pool
-    .query(state.query)
-    .catch((err) => logger.error(loggerOptions, `Error updating account ${state.account.id}: ${err}`));
-
-  return { ...state, queryResult };
-};
-
-/**
- * Runs every iteration. Fetchs some data from the blockchain, constructs a
- * SQL query an executes the query.
- *
- * @param {*} wsProviderUrl   Substrate node WS
- * @param {*} pool            Postgres connection pool
- */
-const exec = async (wsProviderUrl, pool) => {
+const start = async (wsProviderUrl, pool) => {
   logger.info(loggerOptions, 'Running active accounts crawler...');
   const wsProvider = new WsProvider(wsProviderUrl);
   const api = await ApiPromise.create({ provider: wsProvider });
   const startTime = new Date().getTime();
-
-  const timestamp = Date.now();
-  const block = await fetchBlockNumber(api);
   const accountIds = await fetchAccountIds(api);
+  logger.info(loggerOptions, `Got ${accountIds.length} active accounts`);
+  let processed = 0;
 
-  logger.info(loggerOptions, `got ${accountIds.length} accountids`);
-
-  const accountsIdentity = await Promise.all(
-    accountIds.map((id) => fetchAccountIdentity(id, api)),
-  );
-
-  logger.info(loggerOptions, 'got identities');
-
-  const accountsBalances = await Promise.all(
-    accountIds.map((id) => fetchAccountBalance(id, api)),
-  );
-
-  logger.info(loggerOptions, 'got balances');
-
-  await Promise.all(
-    makeState([accountIds, accountsIdentity, accountsBalances])
-      .map((state) => makeQuery(state, block, timestamp))
-      .map((state) => execQuery(state, pool))
-      .map((state) => state.queryResult), // Pick the promise to await
-  );
+  // eslint-disable-next-line no-restricted-syntax
+  for (const accountId of accountIds) {
+    const loopStartTime = Date.now();
+    // eslint-disable-next-line no-await-in-loop
+    const [block, identity, balances] = await Promise.all([
+      api.rpc.chain.getBlock().then((result) => result.block.header.number.toNumber()),
+      api.derive.accounts.identity(accountId),
+      api.derive.balances.all(accountId),
+    ]);
+    const availableBalance = balances.availableBalance.toString();
+    const freeBalance = balances.freeBalance.toString();
+    const lockedBalance = balances.lockedBalance.toString();
+    const identityDisplay = identity.display ? identity.display.toString() : '';
+    const identityDisplayParent = identity.displayParent ? identity.displayParent.toString() : '';
+    const JSONIdentity = identity.display ? JSON.stringify(identity) : '';
+    const JSONbalances = JSON.stringify(balances);
+    const nonce = balances.accountNonce.toString();
+    const data = [
+      accountId,
+      JSONIdentity,
+      identityDisplay,
+      identityDisplayParent,
+      JSONbalances,
+      availableBalance,
+      freeBalance,
+      lockedBalance,
+      nonce,
+      loopStartTime,
+      block,
+    ];
+    const query = `
+      INSERT INTO account (
+        account_id,
+        identity,
+        identity_display,
+        identity_display_parent,
+        balances,
+        available_balance,
+        free_balance,
+        locked_balance,
+        nonce,
+        timestamp,
+        block_height
+      ) VALUES (
+        $1,
+        $2,
+        $3,
+        $4,
+        $5,
+        $6,
+        $7,
+        $8,
+        $9,
+        $10,
+        $11
+      )
+      ON CONFLICT (account_id)
+      DO UPDATE SET
+        identity = EXCLUDED.identity,
+        identity_display = EXCLUDED.identity_display,
+        identity_display_parent = EXCLUDED.identity_display_parent,
+        balances = EXCLUDED.balances,
+        available_balance = EXCLUDED.available_balance,
+        free_balance = EXCLUDED.free_balance,
+        locked_balance = EXCLUDED.locked_balance,
+        nonce = EXCLUDED.nonce,
+        timestamp = EXCLUDED.timestamp,
+        block_height = EXCLUDED.block_height
+    ;`;
+    // eslint-disable-next-line no-await-in-loop
+    await dbParamInsert(pool, query, data, loggerOptions);
+    processed += 1;
+    const loopEndTime = new Date().getTime();
+    logger.info(loggerOptions, `Processed account ${processed} of ${accountIds} (${accountId}) in ${((loopEndTime - loopEndTime) / 1000).toFixed(3)}s`);
+  }
 
   await api.disconnect();
   const endTime = new Date().getTime();
   logger.info(loggerOptions, `Processed ${accountIds.length} active accounts in ${((endTime - startTime) / 1000).toFixed(3)}s`);
-};
-
-/**
- * Calls run function and then sets a timer to run the function again after
- * some time.
- *
- * @param {*} wsProviderUrl     Substrate node WS
- * @param {*} pool              Postgres connection pool
- * @param {*} config            Crawler configuration
- */
-const start = async (wsProviderUrl, pool, config) => {
-  const pollingTime = config.pollingTime || DEFAULT_POLLING_TIME_MS;
-
-  await wait(config.startDelay);
-  logger.info(loggerOptions, 'Starting active accounts crawler...');
-
-  (async function run() {
-    await exec(wsProviderUrl, pool).catch((err) => logger.error(loggerOptions, `Error running crawler: ${err}`));
-    setTimeout(() => run(), pollingTime);
-  }());
 };
 
 module.exports = { start };
