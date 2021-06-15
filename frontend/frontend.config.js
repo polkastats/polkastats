afa3d0e31f7b4928607d9bbaedd08bf866bb4795
mariopino <mariopino@protonmail.com>
2021-06-15 12:21:03 +0200

    include frontend config

---
+++ frontend/frontend.config.js
@@ -0,0 +1,15 @@
+export const network = {
+  id: 'kusama',
+  name: 'Kusama',
+  tokenSymbol: 'KSM',
+  tokenDecimals: 12,
+  ss58Format: 2,
+  coinGeckoDenom: 'kusama',
+  nodeWs: 'wss://kusama-rpc.polkadot.network',
+  backendWs: 'wss://dev.kusama.polkastats.io/api/v3',
+  backendHttp: 'https://dev.kusama.polkastats.io/api/v3',
+  verificatorApi: 'https://testnet.reefscan.com/api/verificator',
+  googleAnalytics: '',
+  theme: '@/assets/scss/themes/polkastats.scss',
+}
+export const paginationOptions = [10, 20, 50, 100]
