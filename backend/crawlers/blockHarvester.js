0a80eb872858e03c2f9d0ae1e34ab0f60f5b50ec
mariopino <mariopino@protonmail.com>
2021-06-15 15:17:24 +0200

    tweak

--- backend/crawlers/blockHarvester.js
+++ backend/crawlers/blockHarvester.js
@@ -22,7 +22,7 @@ const loggerOptions = {
 const config = backendConfig.crawlers.find(
   ({ name }) => name === crawlerName,
 );
-const chunkSize = 10;
+const chunkSize = 100;
 
 const chunker = (a, n) => Array.from(
   { length: Math.ceil(a.length / n) },
