32a8a2ecb5fd04adda8279895078903011a4e088
mariopino <mariopino@protonmail.com>
2021-06-15 10:43:16 +0200

    front conf

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 export const config = {
   name: 'kusama',
-  title: 'Validator Resource Center and Ranking',
+  title: 'PolkaStats NG',
   nodeWs: 'wss://kusama-rpc.polkadot.io',
   denom: 'KSM',
   addressPrefix: 2,
@@ -13,7 +13,7 @@ export const config = {
   favicon: 'img/favicon/kusama.ico',
   baseURL: '/',
   showValSelectorInPage: false, // set to false when showing val selector in header
-  googleAnalytics: 'G-0MDQV4GFD9',
-  backendWs: 'wss://validatorsv2.kusama.polkastats.io/api/v3',
-  backendHttp: 'https://validatorsv2.kusama.polkastats.io/api/v3',
+  googleAnalytics: null,
+  backendWs: 'wss://dev.kusama.polkastats.io/api/v3',
+  backendHttp: 'https://dev.kusama.polkastats.io/api/v3',
 }
