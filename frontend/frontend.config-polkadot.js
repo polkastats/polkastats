9be090dbbae3fb950e47ddf50c4119fd14150a21
mariopino <mariopino@protonmail.com>
2021-06-23 18:30:36 +0200

    frontend config file

---
+++ frontend/frontend.config-polkadot.js
@@ -0,0 +1,14 @@
+export const network = {
+  id: 'polkadot',
+  name: 'Polkadot',
+  tokenSymbol: 'DOT',
+  tokenDecimals: 10,
+  ss58Format: 0,
+  coinGeckoDenom: 'polkadot',
+  nodeWs: 'wss://rpc.polkadot.network',
+  backendWs: 'wss://dev.polkadot.polkastats.io/api/v3',
+  backendHttp: 'https://dev.polkadot.polkastats.io/api/v3',
+  googleAnalytics: '',
+  theme: '@/assets/scss/themes/polkastats.scss',
+}
+export const paginationOptions = [10, 20, 50, 100]
