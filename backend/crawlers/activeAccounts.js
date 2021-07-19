4615ec5c126157455eee1018f7853eedce225c31
mariopino <mariopino@protonmail.com>
2021-07-19 19:14:03 +0200

    increase sleep time

--- backend/crawlers/activeAccounts.js
+++ backend/crawlers/activeAccounts.js
@@ -142,7 +142,7 @@ const crawler = async (delayedStart) => {
     const chunkEndTime = new Date().getTime();
     logger.info(loggerOptions, `Processed chunk ${chunks.indexOf(chunk) + 1}/${chunks.length} in ${((chunkEndTime - chunkStartTime) / 1000).toFixed(3)}s`);
     // eslint-disable-next-line no-await-in-loop
-    await wait(100);
+    await wait(200);
   }
 
   await api.disconnect();
