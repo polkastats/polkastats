6d32fe913bfddcc12c49f7c739e96c0bcddc827d
mariopino <mariopino@protonmail.com>
2021-08-17 20:10:53 +0200

    council app api

--- api/index.js
+++ api/index.js
@@ -128,8 +128,8 @@ app.get('/api/v1/batsignal/system.remarks', async (req, res) => {
         return {
           block_number: parseInt(row.block_number),
           extrinsic_hash: row.hash,
-          args: row.args,
-          decoded_remark: hexToUtf8(JSON.parse(row.args)[0]),
+          // args: row.args,
+          remark: hexToUtf8(JSON.parse(row.args)[0]),
           datetime: moment.unix(row.timestamp).format(), // 2021-08-06T13:53:18+00:00
         }
       });
