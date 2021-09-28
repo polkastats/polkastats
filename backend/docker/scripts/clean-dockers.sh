cfc27208a30393660c88ee759121503a1b8930b9
Andrei Navoichyk <andrei@cere.io>
2021-09-28 16:08:14 +0300

    added backend_api_1 to docker scripts

--- backend/docker/scripts/clean-dockers.sh
+++ backend/docker/scripts/clean-dockers.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+docker stop backend_api_1
 docker stop backend_crawler_1
 docker stop backend_postgres_1
 docker stop backend_graphql-engine_1
 
+docker rm backend_api_1
 docker rm backend_crawler_1
 docker rm backend_postgres_1
 docker rm backend_graphql-engine_1
