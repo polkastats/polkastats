20bc1e921bc1f4825abce3929d42c7a04f00fd32
Karthikeyan <karthikeyan@cere.io>
2021-09-28 17:24:26 +0530

    refactor(header): removed unused code

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -7,28 +7,12 @@
           Cerestats
         </nuxt-link>
       </b-navbar-brand>
-      <!-- <a
-        v-if="network.coinGeckoDenom && USDConversion && USD24hChange"
-        :href="`https://www.coingecko.com/en/coins/${network.coinGeckoDenom}`"
-        target="_blank"
-        class="fiat mh-2"
-      >
-        <strong>{{ network.tokenSymbol }}</strong> ${{ USDConversion }} ({{
-          USD24hChange
-        }}%)
-      </a> -->
       <b-navbar-toggle target="nav-collapse" />
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <!-- <b-nav-item right to="/polkastats-validator">{{
-            $t('layout.default.validator')
-          }}</b-nav-item> -->
           <b-nav-item right to="/validators">{{
             $t('layout.default.validators')
           }}</b-nav-item>
-          <!-- <b-nav-item right to="/how-to-stake">{{
-            $t('layout.default.how_to_stake')
-          }}</b-nav-item> -->
           <b-nav-item-dropdown text="Blockchain">
             <b-dropdown-item to="/blocks">
               {{ $t('layout.default.blocks') }}
@@ -47,23 +31,6 @@
             $t('layout.default.accounts')
           }}</b-nav-item>
         </b-navbar-nav>
-        <!-- <b-dropdown class="my-md-2 ml-md-2 network" variant="primary2">
-          <template #button-content>
-            <img
-              class="network-logo"
-              :src="`/img/networks/icons/${network.id}.svg`"
-            />
-            {{ network.name }}
-          </template>
-          <b-dropdown-item href="https://kusama.polkastats.io">
-            <img class="network-logo" src="/img/networks/icons/kusama.svg" />
-            KUSAMA
-          </b-dropdown-item>
-          <b-dropdown-item href="https://polkastats.io">
-            <img class="network-logo" src="/img/networks/icons/polkadot.svg" />
-            POLKADOT
-          </b-dropdown-item>
-        </b-dropdown> -->
       </b-collapse>
     </b-container>
   </b-navbar>
