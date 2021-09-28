519a8338cb0641c2c8afdf6a51f535559499ff46
Andrei Navoichyk <andrei@cere.io>
2021-09-28 18:04:56 +0300

    added backend_api container management

--- backend/docker/scripts/backup.sh
+++ backend/docker/scripts/backup.sh
@@ -4,16 +4,17 @@ mkdir -p ~/backup
 cd ~/backup
 
 # Stop all cointainers
+docker stop backend_api_1
 docker stop backend_crawler_1
 docker stop backend_postgres_1
 docker stop backend_graphql-engine_1
 docker stop backend_substrate-node_1
 sleep 5
 
-if [ $(docker inspect -f '{{.State.Running}}' backend_substrate-node_1) = "true" ]; then 
+if [ $(docker inspect -f '{{.State.Running}}' backend_substrate-node_1) = "true" ]; then
     echo "Substrate node still running"
     exit 1
-else 
+else
     echo "Substrate node stopped"
     echo "Starting the backup"
 fi
@@ -29,3 +30,4 @@ docker start backend_substrate-node_1
 docker start backend_postgres_1
 docker start backend_graphql-engine_1
 docker start backend_crawler_1
+docker start backend_api_1
