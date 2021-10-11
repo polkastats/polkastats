fcb82de880ab1b037e8a9d50d76ca681b46b1261
karthikeyan-cere <73122026+karthikeyan-cere@users.noreply.github.com>
2021-10-11 16:14:29 +0530

    Feature/fix transfer counts (#10)
    * fix(title): undo commit
    
    * fix(sql): fixed transfer counts
    
    * fix(sql): updated trigger
    
    * refactor(sql): optimise query
    
    * fix(sql): updated else block in trigger

--- backend/docker/sql/backend-dev.sql
+++ backend/docker/sql/backend-dev.sql
@@ -334,3 +334,32 @@ CREATE TRIGGER event_count_trunc AFTER TRUNCATE ON event
 -- initialize the counter table
 UPDATE total SET count = (SELECT count(*) FROM event) WHERE name = 'events';
 COMMIT;
+
+-- Transfers
+START TRANSACTION;
+CREATE FUNCTION transfer_count() RETURNS trigger LANGUAGE plpgsql AS
+$$BEGIN
+  IF NEW.method IN ('transfer', 'transferKeepAlive') THEN
+    IF TG_OP = 'INSERT' THEN
+      UPDATE total SET count = count + 1 WHERE name = 'transfers';
+      RETURN NEW;
+    ELSIF TG_OP = 'DELETE' THEN
+      UPDATE total SET count = count - 1 WHERE name = 'transfers';
+      RETURN OLD;
+    ELSE
+      UPDATE total SET count = 0 WHERE name = 'transfers';
+      RETURN NULL;
+    END IF;
+  END IF;
+  RETURN NULL;
+END;$$;
+CREATE CONSTRAINT TRIGGER transfer_count_mod
+  AFTER INSERT OR DELETE ON extrinsic
+  DEFERRABLE INITIALLY DEFERRED
+  FOR EACH ROW EXECUTE PROCEDURE transfer_count();
+-- TRUNCATE triggers must be FOR EACH STATEMENT
+CREATE TRIGGER transfer_count_trunc AFTER TRUNCATE ON extrinsic
+  FOR EACH STATEMENT EXECUTE PROCEDURE transfer_count();
+-- initialize the counter table
+UPDATE total SET count = (SELECT count(*) FROM extrinsic WHERE method IN ('transfer', 'transferKeepAlive')) WHERE name = 'transfers';
+COMMIT;
