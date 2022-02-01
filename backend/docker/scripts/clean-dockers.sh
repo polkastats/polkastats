8b0803102742d5e5c70f8b02fd52d4fd5edba46f
mariopino <mariopino@protonmail.com>
2022-02-01 15:25:59 +0100

    add api to docker clean script

--- backend/docker/scripts/clean-dockers.sh
+++ backend/docker/scripts/clean-dockers.sh
@@ -3,6 +3,7 @@ docker stop backend_crawler_1
 docker stop backend_postgres_1
 docker stop backend_graphql-engine_1
 
+docker rm backend_api_1
 docker rm backend_crawler_1
 docker rm backend_postgres_1
 docker rm backend_graphql-engine_1
