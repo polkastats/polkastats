bc98b57e9a4f07ca4fcdac07558662706d026500
mariopino <mariopino@protonmail.com>
2022-02-20 09:44:53 +0100

    debug

--- backend/test.js
+++ backend/test.js
@@ -1,5 +1,6 @@
 // Required imports
-const { ApiPromise, WsProvider } = require('@polkadot/api');
+import '@polkadot/api-augment';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
 async function main() {
   // Initialise the provider to connect to the local node
