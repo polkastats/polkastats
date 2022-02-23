857472de4b9dbc978e5fdf7d10bf61e5a63eb297
mariopino <mariopino@protonmail.com>
2022-02-23 19:36:26 +0100

    fix api sidecar endpoint

--- backend/src/backend.config.ts
+++ backend/src/backend.config.ts
@@ -13,7 +13,7 @@ export const backendConfig = {
   },
   logLevel: process.env.LOG_LEVEL || 'info',
   sentryDSN: process.env.SENTRY_DSN || '',
-  substrateApiSidecar: 'ws://substrate-node:9090',
+  substrateApiSidecar: 'http://sidecar:9090',
   crawlers: [
     {
       name: 'blockListener',
