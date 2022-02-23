5fe50bffb325a339bfafb4b67c5a2afad0aaa3b5
mariopino <mariopino@protonmail.com>
2022-02-23 20:05:12 +0100

    fix runtime table

--- backend/docker/sql/backend.sql
+++ backend/docker/sql/backend.sql
@@ -279,7 +279,7 @@ CREATE TABLE IF NOT EXISTS runtime (
   block_number BIGINT NOT NULL,
   spec_name TEXT NOT NULL,
   spec_version INT NOT NULL,
-  metadata_version JSON NOT NULL,
+  metadata_version TEXT NOT NULL,
   metadata_magic_number INT NOT NULL,
   metadata JSON NOT NULL,
   timestamp BIGINT NOT NULL,
