8cebb020c7910a487c0365444c3d851c43304005
Andrei Navoichyk <andrei@cere.io>
2022-03-08 18:09:28 +0600

    renamed parameter

--- api/src/services/ethNetworkService.js
+++ api/src/services/ethNetworkService.js
@@ -1,6 +1,6 @@
 const Web3 = require('web3');
-const { ETHEREUM_HTTP_PROVIDER, ETHEREUM_CERE_TOKEN_CONTRACT_ADDRESS } = process.env;
-const web3 = new Web3(new Web3.providers.HttpProvider(ETHEREUM_HTTP_PROVIDER));
+const { ETHEREUM_HTTP_PROVIDER_URL, ETHEREUM_CERE_TOKEN_CONTRACT_ADDRESS } = process.env;
+const web3 = new Web3(new Web3.providers.HttpProvider(ETHEREUM_HTTP_PROVIDER_URL));
 const erc20TokenAbi = require('./../contracts/erc20TokenAbi.json');
 const { decimal } = require("./../constants/config");
 const contract = new web3.eth.Contract(
