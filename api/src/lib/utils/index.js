6e2c06e98f00412e8e865eaf759829ea607ffd73
Dmitriy Molchanov <dmolchanov@upteam.com>
2022-04-19 15:55:47 +0300

    Fix metrics response to  be a float number

--- api/src/lib/utils/index.js
+++ api/src/lib/utils/index.js
@@ -1,11 +1,6 @@
 const web3 = require('web3');
 const { BN } = web3.utils;
 
-function convertDecimalsToCoins (amount, decimals) {
-    const base  = new BN(10);
-    return amount.div(base.pow(decimals));
-}
-
 function getTokenFloatAmount(amount, decimals){
     const base = new BN(10);    
     const float = amount.divmod(base.pow(decimals));
@@ -13,6 +8,5 @@ function getTokenFloatAmount(amount, decimals){
 } 
 
 module.exports = {
-    convertDecimalsToCoins,
     getTokenFloatAmount
 }
--- api/src/services/metricsService.js
+++ api/src/services/metricsService.js
@@ -1,11 +1,11 @@
 const prom = require('prom-client');
 const { decimals } = require('../config/blockchains');
 const cacheService = require('./cacheService');
-const { convertDecimalsToCoins } = require('../lib/utils')
+const { getTokenFloatAmount } = require('../lib/utils')
 
 const accountsBalancesMetric = new prom.Gauge({
-  name: 'accounts_balances',
-  help: 'accounts balances amount',
+  name: 'blockchains_accounts_balances',
+  help: 'blockchains accounts balances from Cere Stats',
   labelNames: ['blockchain', 'network', 'name', 'address', 'tokenSymbol', 'group'],
 });
 
@@ -16,7 +16,7 @@ module.exports = {
       const { blockchain , network, name, address, tokenSymbol, group, balance } = account;
       accountsBalancesMetric
         .labels({ blockchain, network, name, address, tokenSymbol, group })
-        .set(+convertDecimalsToCoins(balance, decimals.CERE));
+        .set(getTokenFloatAmount(balance, decimals[tokenSymbol]));
     });
     res.set('Content-Type', prom.register.contentType);
     res.end(await prom.register.metrics());
