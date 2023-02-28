ee8345401f4b54b58785aba88588a998d0ca6d93
Shamilkhan Akhmetzyanov <shamilkhan.akhmetzyanov@cere.io>
2023-02-28 18:08:31 +0400

    feat(db): add ss58 migration for account table

---
+++ db/migrations/20230227122704-ss58-prefix.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+var keyring = require('@polkadot/keyring');
+var Promise;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
+    Promise = options.Promise;
+};
+
+const convertAccountTable = (db, ss58Format) => {
+    return db.runSql('SELECT * from account;', (_, result) => {
+
+        result.rows.forEach((row) => {
+            const currentAccountId = row.account_id;
+
+            const nextAddress = keyring.encodeAddress(
+                keyring.decodeAddress(currentAccountId),
+                ss58Format);
+
+            const queryString = `UPDATE account SET account_id='${nextAddress}' WHERE account_id='${currentAccountId}';`
+
+            db.runSql(queryString);
+        });
+
+    });
+}
+
+exports.up = function (db) {
+    return convertAccountTable(db, 54);
+};
+
+exports.down = function (db) {
+    return convertAccountTable(db, 42);
+};
+
+exports._meta = {
+    "version": 1
+};
