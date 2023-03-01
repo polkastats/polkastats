b3dd4fad8137d0f211f47414a83f8159fad0fc09
Shamilkhan Akhmetzyanov <shamilkhan.akhmetzyanov@cere.io>
2023-03-01 14:41:33 +0400

    feat(db): add migration for era_* and featured tables

--- db/migrations/20230227122704-ss58-prefix.js
+++ db/migrations/20230227122704-ss58-prefix.js
@@ -17,6 +17,7 @@ exports.setup = function (options, seedLink) {
     Promise = options.Promise;
 };
 
+// Migration script for Account Table
 const convertAccountTable = (db, ss58Format) => {
     return db.runSql('SELECT * from account;', (_, result) => {
 
@@ -35,15 +36,47 @@ const convertAccountTable = (db, ss58Format) => {
     });
 }
 
+// Migration script for featured, era_commission, era_points, era_relative_performance, era_self_stake, era_vrc_score tables
+const convertEraTables = (db, ss58Format) => {
+    const tables = [
+        'featured',
+        'era_commission',
+        'era_points',
+        'era_relative_performance',
+        'era_self_stake',
+        'era_vrc_score',
+    ];
+    tables.forEach((table) => {
+        db.runSql(`SELECT * from ${table}`, (_, result) => {
+
+            result.rows.forEach((row) => {
+                const currentAccountId = row.stash_address;
+
+                const nextAddress = keyring.encodeAddress(
+                    keyring.decodeAddress(currentAccountId),
+                    ss58Format);
+
+                const queryString = `UPDATE ${table} SET stash_address='${nextAddress}' WHERE stash_address='${currentAccountId}';`
+
+                db.runSql(queryString);
+
+            });
+
+        });
+    });
+};
+
 exports.up = function (db) {
     db.startMigration();
     convertAccountTable(db, 54);
+    convertEraTables(db, 54);
     return db.endMigration();
 };
 
 exports.down = function (db) {
     db.startMigration();
     convertAccountTable(db, 42);
+    convertEraTables(db, 42);
     return db.endMigration();
 };
 
