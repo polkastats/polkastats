563509384a1ff35cca9d1ad071310a2f57dcb545
shamilkhan <shamilkhan1999@gmail.com>
2023-04-03 15:16:25 +0400

    Create shared folder for ss58-prefix migration

--- db/migrations/20230227122704-3-ss58-prefix.js
+++ db/migrations/20230227122704-3-ss58-prefix.js
@@ -3,7 +3,13 @@
 var dbm;
 var type;
 var seed;
-var keyring = require('@polkadot/keyring');
+const {
+    SS58_PREFIX_OLD,
+    SS58_PREFIX_NEW,
+    executeDbRunSqlAsPromise,
+    Logger,
+    decode,
+} = require('./shared/ss58-prefix/index.js');
 var Promise;
 
 /**
@@ -183,69 +189,12 @@ const convertTables = async (db, prefix) => {
     await convertTransfers(db, prefix);
 }
 
-class Logger {
-    constructor(tableName) {
-        this.tableName = tableName;
-        this.totalRows = 0;
-        this.currentRow = 0;
-        this.migratedPercent = 0;
-        console.log(`Migrating ${this.tableName}`);
-    }
-
-    setTotalRows = (totalRows) => {
-        this.totalRows = totalRows;
-        console.log(`Total Rows are ${this.tableName}`);
-    }
-
-    log = () => {
-        this.currentRow++;
-        const {currentRow, totalRows, tableName} = this;
-
-        if (currentRow === totalRows) {
-            console.log(`✅ Done for ${tableName} table`);
-        } else {
-            const migratedPercent = Math.trunc(100 * currentRow / totalRows);
-
-            if (migratedPercent % 20 === 0 && migratedPercent > this.migratedPercent) {
-                this.migratedPercent = migratedPercent;
-                console.log(`⏳ Migrated ${migratedPercent}% for ${tableName} table`);
-            }
-        }
-    }
-}
-
-const decode = (address, ss58) => {
-    if (address.startsWith('0x')) {
-        console.log('Address starts with 0x', address);
-        return address;
-    } else {
-        return keyring.encodeAddress(
-            keyring.decodeAddress(address),
-            ss58);
-    }
-};
-
-const executeDbRunSqlAsPromise = (db, sqlQuery) => {
-    return new Promise(function (resolve, reject) {
-        db.runSql(sqlQuery, (error, result) => {
-            if (error) {
-                console.log('Error', error);
-                console.log('sqlQuery', sqlQuery);
-                reject(error);
-            }
-            resolve(result);
-        })
-    });
-}
-
 exports.up = async function (db) {
-    const newSs58Prefix = 54;
-    return convertTables(db, newSs58Prefix);
+    return convertTables(db, SS58_PREFIX_NEW);
 };
 
 exports.down = async function (db) {
-    const oldSs58Prefix = 42;
-    return convertTables(db, oldSs58Prefix);
+    return convertTables(db, SS58_PREFIX_OLD);
 };
 
 exports._meta = {
