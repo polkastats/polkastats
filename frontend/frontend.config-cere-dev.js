b475d2341a180e350e18812f701db0f285dcc0b5
shamilkhan <shamilkhan1999@gmail.com>
2023-07-04 15:09:07 +0400

    Remove googleAnalytics from Dev.

--- frontend/frontend.config-cere-dev.js
+++ frontend/frontend.config-cere-dev.js
@@ -8,7 +8,7 @@ export const network = {
   nodeWs: 'wss://archive.testnet.cere.network/ws',
   backendWs: 'wss://hasura.stats.dev.cere.network/v1/graphql',
   backendHttp: 'https://api.stats.dev.cere.network/api/v1',
-  googleAnalytics: 'GTM-KC59ZLM',
+  googleAnalytics: '',
   theme: '@/assets/scss/themes/polkastats.scss',
   // ranking               Í
   historySize: 16, // 16 days
